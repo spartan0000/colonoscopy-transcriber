a802f9359b7ae9ee6a45412fb370390c6fc18413
David Lee <doctor.dlee@gmail.com>
2026-04-28 20:48:00 +1200

    add logger

--- app/api/transcription_route.py
+++ app/api/transcription_route.py
@@ -4,6 +4,8 @@ from app.database.connection import get_db
 from app.services import functions
 
 from app.models.colonoscopy import ColonoscopyReport
+from app.logger import logger
+
 
 from sqlalchemy.orm import Session
 
@@ -19,8 +21,6 @@ def get_draft_id():
 router = APIRouter(tags=['transcription'])
 
 
-#need to remove all the logging in the function below at some point
-
 @router.post("/transcribe")
 async def transcribe(cecum_reached_time: datetime | None = Form(None), procedure_end_time: datetime | None = Form(None), file: UploadFile = File(...)):
     """
@@ -28,15 +28,16 @@ async def transcribe(cecum_reached_time: datetime | None = Form(None), procedure
     """
     print("CECUM_DEBUG:  ", cecum_reached_time)
     print("PROCEDURE END TIME DEBUG:  ", procedure_end_time)
-    
+    logger.info(f"Received cecum reached time: {cecum_reached_time}")
+    logger.info(f"Received procedure end time: {procedure_end_time}")
 
     ###Logging the input to the endpoint
     contents = await file.read()
-    print("\n----UPLOAD DEBUG---")
-    print(f"FILENAME: {file.filename}")
-    print(f"CONTENT TYPE: {file.content_type}")
-    print(f"SIZE: {len(contents)}")
-    print("--------------\n")
+    # print("\n----UPLOAD DEBUG---")
+    # print(f"FILENAME: {file.filename}")
+    # print(f"CONTENT TYPE: {file.content_type}")
+    # print(f"SIZE: {len(contents)}")
+    # print("--------------\n")
 
     file.file.seek(0)
     ###
@@ -49,22 +50,29 @@ async def transcribe(cecum_reached_time: datetime | None = Form(None), procedure
     ###Logging what comes back from the transcribe LLM
     print("\n---TRANSCRIPTION OUTPUT---")
     print(f"TRANSCRIPT: {transcription_result}")
-    ###
+    ##################################################################
+    
+    
+    logger.info(f"Transcription with timestamps: {transcription_result}")
 
     extracted_data, status = await functions.extract_json(transcription_result)
 
-    #Logging what comes back from the chat LLM
+    
+    #Logging what comes back from the chat LLM #######################
     print("\n---LLM OUTPUT---")
     print(f"LLM OUTPUT: {extracted_data}")
+    logger.info(f"LLM extracted structured data: {extracted_data}")
+    ###################################################################
 
-
+    
     extracted_data_with_timestamps = functions.add_time_stamps(extracted_data, cecum_reached_time, procedure_end_time)
 
+    logger.info(f"Added time stamps: {extracted_data_with_timestamps}")
 
     full_report = functions.generate_fake_data(extracted_data_with_timestamps) #fake data for now, replace with real metadata extraction
 
-    #functions.write_transcription_record(db=db, full_report = full_report) #move this function to the write_db_generate_pdf route
-
+    logger.info(f"Full report: {full_report}")
+    
 
 
     return {
--- app/config.py
+++ app/config.py
@@ -1,6 +1,14 @@
 from pathlib import Path
 import os
 
+import logging
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 API_BASE = os.getenv("API_BASE_URL")
 
 BASE_DIR = Path(__file__).resolve().parent
