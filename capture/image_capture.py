4e9ef1401b2b6c94d3d13785115607d1410e4504
David Lee <doctor.dlee@gmail.com>
2026-06-29 15:48:29 +1200

    refactor image capture function

--- capture/image_capture.py
+++ capture/image_capture.py
@@ -5,12 +5,28 @@ import pathlib
 import requests
 
 
+OUTPUT_FOLDER = "captured_images"
 
+def save_frame_locally(frame, output_folder, timestamp):
+    """save frame to local file system and returns filename"""
+
+    os.makedirs(output_folder, exist_ok=True)
+    filename = os.path.join(output_folder, f"endoscope_{timestamp}.png")
+    cv2.imwrite(filename, frame)
+    return filename
+
+def upload_image(filename, transcript_id):
+    with open(filename, 'rb') as f:
+        response = requests.post(
+            "/transcripts/{transcript_id}/images",
+            file = {"image": (os.path.basename(filename), f, 'image/png')},
+            data = {"captured_at":time.strftime("%Y%m%d_%H%M%S")})
+
+        response.raise_for_status()
+        return response.json()
+    
 
 def run_trigger_capture(transcript_id: int, device_index:int = 0):
-    output_folder = "captured_images"
-
-    os.makedirs(output_folder, exist_ok = True)
 
     cap = cv2.VideoCapture(device_index)
 
@@ -31,19 +47,13 @@ def run_trigger_capture(transcript_id: int, device_index:int = 0):
         elif key == 32:
             print(f"Space key detected: {key}")
             timestamp = time.strftime("%Y%m%d_%H%M%S")
-            filename = os.path.join(output_folder, f"endoscope_{timestamp}.png")
+            filename = save_frame_locally(frame, OUTPUT_FOLDER, timestamp)
             cv2.imwrite(filename, frame)
             print(f"[Trigger detected] Saved image: {filename}") #images now saved locally
 
             try: #sending images to fastapi server
-                with open(filename, "rb") as f:
-                    response = requests.post(
-                        f"/transcripts/{transcript_id}/images",
-                        files = {"image":(os.path.basename(filename), f, "image/png" )}, 
-                        data = {"captured_at":timestamp}
-                        )
-                    response.raise_for_status()
-                    print(f"Uploaded: image id: {response.json()['image_id']}")
+                result = upload_image(filename, transcript_id)
+                print(f"Uploaded: image id: {result['image_id']}")
             except requests.RequestException as e:
                 print(f"Upload failed.  Image saved locally: {e}")
 
@@ -52,4 +62,4 @@ def run_trigger_capture(transcript_id: int, device_index:int = 0):
 
 if __name__ == "__main__":
     transcript_id = int(input("enter transcript_id: "))
-    run_trigger_capture(transcript_id)
+    run_trigger_capture(transcript_id=transcript_id)
